7b9096db1de49df38d4ebc814165de5a59b626fb
rajkiranas@gmail.com <rajkiranas@gmail.com@48afb5ea-cf32-f654-0787-a7c2e7c88f2e>
2014-04-29 07:01:51 +0000

    illness entiry and bean changed - as new columns added

--- src/main/java/com/srti/gbb/bean/IllnessBean.java
+++ src/main/java/com/srti/gbb/bean/IllnessBean.java
@@ -15,6 +15,12 @@ public class IllnessBean  implements Serializable
 {
     private String illness;
     private boolean isHospitalized;
+    private short intensity;
+    private short frequency;
+    private float duration;
+    private short lastsForDays;
+    private short sinceYears;
+    private short lossOfManDays;
 
     public String getIllness() {
         return illness;
@@ -56,7 +62,57 @@ public class IllnessBean  implements Serializable
 
     @Override
     public String toString() {
-        return "IllnessBean{" + "illness=" + illness + ", isHospitalized=" + isHospitalized + '}';
+        return "IllnessBean{" + "illness=" + illness + ", isHospitalized=" + isHospitalized + ", intensity=" + intensity + ", frequency=" + frequency + ", duration=" + duration + ", lastsForDays=" + lastsForDays + ", sinceYears=" + sinceYears + ", lossOfManDays=" + lossOfManDays + '}';
+    }
+
+    
+
+    public short getIntensity() {
+        return intensity;
+    }
+
+    public void setIntensity(short intensity) {
+        this.intensity = intensity;
+    }
+
+    public short getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(short frequency) {
+        this.frequency = frequency;
+    }
+
+    public float getDuration() {
+        return duration;
+    }
+
+    public void setDuration(float duration) {
+        this.duration = duration;
+    }
+
+    public short getLastsForDays() {
+        return lastsForDays;
+    }
+
+    public void setLastsForDays(short lastsForDays) {
+        this.lastsForDays = lastsForDays;
+    }
+
+    public short getSinceYears() {
+        return sinceYears;
+    }
+
+    public void setSinceYears(short sinceYears) {
+        this.sinceYears = sinceYears;
+    }
+
+    public short getLossOfManDays() {
+        return lossOfManDays;
+    }
+
+    public void setLossOfManDays(short lossOfManDays) {
+        this.lossOfManDays = lossOfManDays;
     }
     
     
